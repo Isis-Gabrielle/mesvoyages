63e5abb3e0ded6930441a67ec7f66323b2f5107f
Arlène <dogacademy666@gmail.com>
2025-09-03 11:05:18 +0200

    2ème phase: Test

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,15 @@
+<?php
+
+  
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController {
+
+    #[Route('/', name: 'accueil')]
+    public function index(): Response{
+        return new Response('Test');
+    }
+}
